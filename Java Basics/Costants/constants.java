b3a108633038e83561b99fff4862e3dbfdaef989
Utkarsh Gupta <utkarshgupta3008@gmail.com>
2024-03-03 20:34:54 +0530

    Constants

---
+++ Java Basics/Costants/constants.java
@@ -0,0 +1,9 @@
+// Introduction to constants
+
+public class constants {
+  public static void main(String[] args) {
+    final int PRICE = 230;
+    // PRICE = 211; // will give error
+    System.out.println(PRICE);
+  }
+}
