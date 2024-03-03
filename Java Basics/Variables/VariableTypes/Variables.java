998915f63e6e71e68dc7a790f231b6d6a09cff8b
Utkarsh Gupta <utkarshgupta3008@gmail.com>
2024-03-03 17:55:35 +0530

    Variables

---
+++ Java Basics/Variables/VariableTypes/Variables.java
@@ -0,0 +1,27 @@
+// Introduction to JAVA Variables
+/*
+Types of Variables : 
+1) Primitive Variables                        | 2) Non-Primitive Types
+-> byte       {takes 1 byte of space}         | -> Strings (immutable)
+-> short      {takes 2 bytes of space}        | -> Arrays
+-> int        {takes 4 bytes of space}        |
+-> long       {takes 8 bytes of space}        |
+-> float      {takes 4 bytes of space}        |
+-> double     {takes 8 bytes of space}        |
+-> char       {takes 2 bytes of space}        |
+-> boolean    {takes 1 byte of space}         |
+ */
+
+public class Variables {
+  public static void main(String[] args) {
+    // Primitive
+    byte age = 30;
+    int number = 1234567890;
+    long number2 = 123456789089210L; // write letter "L" so jre recognises it as long variable tupe
+    float pi = 3.14F; // same with float
+    double accelearation = 9.8;
+    char dollar = '$';
+    boolean isAdult = true;
+
+  }
+}
