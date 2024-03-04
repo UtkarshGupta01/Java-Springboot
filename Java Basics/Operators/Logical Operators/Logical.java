34496dea5b2fef063907090d3fbe3d1ed03b828d
Utkarsh Gupta <utkarshgupta3008@gmail.com>
2024-03-04 16:55:56 +0530

    Conditional and logical Operators

---
+++ Java Basics/Operators/Logical Operators/Logical.java
@@ -0,0 +1,14 @@
+// Introduction to Logical Operators
+
+public class Logical {
+  public static void main(String[] args) {
+    boolean a = true;
+    boolean b = false;
+
+    System.out.println(a && b);
+
+    System.out.println(a || b);
+
+    System.out.println(!b);
+  }
+}
