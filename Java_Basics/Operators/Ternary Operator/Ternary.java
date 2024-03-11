2792eb5e850058c026392641ac9e673f0686e9f5
Utkarsh Gupta <utkarshgupta3008@gmail.com>
2024-03-11 10:03:48 +0530

    Ternary Operator

---
+++ Java_Basics/Operators/Ternary Operator/Ternary.java
@@ -0,0 +1,11 @@
+public class Ternary {
+  public static void main(String[] args) {
+    int num1 = 100;
+    int num2 = 210;
+
+    int greaternum = num1 > num2 ? num1 : num2;
+    System.out.println("Greater num is : " + greaternum);
+
+  }
+
+}
