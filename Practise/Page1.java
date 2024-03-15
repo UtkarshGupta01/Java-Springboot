32f84bad4a3c19e633ea09bdf290f6c02875c423
Utkarsh Gupta <utkarshgupta3008@gmail.com>
2024-03-15 14:29:16 +0530

    Practise Questions

---
+++ Practise/Page1.java
@@ -0,0 +1,132 @@
+// Multiplication Table
+
+// import java.util.Scanner;
+// public class Page1 {
+// public static void main(String[] args) {
+// Scanner input = new Scanner(System.in);
+// System.out.print("Enter a number you want table of : ");
+// int table_no = input.nextInt();
+// System.out.println("Table of " + table_no + " : ");
+// table(table_no);
+// input.close();s
+// }
+// static void table(int num) {
+// for (int i = 1; i <= 10; i++) {
+// System.out.println(num + " x " + i + " = " + num * i);
+// }
+// }
+// }
+
+// ==================================================================================
+
+// Sum of all odd numbers
+
+// import java.util.Scanner;
+
+// public class Page1 {
+// public static void main(String[] args) {
+// Scanner input = new Scanner(System.in);
+// System.out.print("Enter a number : ");
+// int num = input.nextInt();
+// input.close();
+// int sum = sumOddNumbers(num);
+// System.out.println("Sum of all odd numbers from 1 to " + num + " is " + sum);
+// }
+
+// static int sumOddNumbers(int num) {
+// int i = 1;
+// int sum = 0;
+// while (i <= num) {
+// sum += i;
+// i += 2;
+
+// }
+// return sum;
+// }
+// }
+
+// ===========================================================================
+
+// Calculate Factorial
+
+// import java.util.Scanner;
+
+// public class Page1 {
+// public static void main(String[] args) {
+// Scanner input = new Scanner(System.in);
+// System.out.print("Enter a number you want the factorial of : ");
+// int num = input.nextInt();
+// System.out.println(num + "! : " + factorial(num));
+// }
+// static int factorial(int n) {
+// int fact = 1;
+// int i = 1;
+// if (n == 1) {
+// return fact;
+// }
+// while (i <= n) {
+// fact *= i;
+// i++;
+// }
+// return fact;
+
+// }
+// }
+
+// ===============================================================================
+
+// Sum of digits of an integer
+
+// import java.util.Scanner;
+
+// public class Page1 {
+// public static void main(String[] args) {
+// Scanner input = new Scanner(System.in);
+// System.out.print("Enter a number to find sum of its digits : ");
+// int num = input.nextInt();
+// System.out.println("Sum of digits of " + num + " : " + sum(num));
+// }
+// static int sum(int n) {
+// int digit = 0;
+// int sum = 0;
+// while (n != 0) {
+// digit = n % 10;
+// n = n / 10;
+// sum += digit;
+// }
+// return sum;
+// }
+// }
+
+// =============================================================================
+
+// Prime Number Or not
+
+import java.util.Scanner;
+
+public class Page1 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a number to check whether its prime or not : ");
+    int num = input.nextInt();
+    if (primeCheck(num)) {
+      System.out.println("It is a prime number.");
+    } else {
+      System.out.println("it is not a prime number. ");
+    }
+    input.close();
+
+  }
+
+  static boolean primeCheck(int n) {
+    for (int i = 2; i < n; i++) {
+      if (n % i == 0) {
+        return false;
+      }
+
+    }
+    return true;
+
+  }
+
+}
