a35201c430fc314177816fe32758f8fda63395d4
Utkarsh Gupta <utkarshgupta3008@gmail.com>
2024-03-15 16:09:16 +0530

    Pattern 2

--- Practise/Page1.java
+++ Practise/Page1.java
@@ -258,3 +258,29 @@
 // }
 
 // --------------------------
+
+// ***
+// **
+// *
+
+import java.util.Scanner;
+
+public class Page1 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter the length of pattern : ");
+    int num = input.nextInt();
+    pattern(num);
+    input.close();
+  }
+
+  static void pattern(int n) {
+    for (int i = n; i >= 1; i--) {
+      for (int j = 1; j <= i; j++) {
+        System.out.print("*");
+      }
+      System.out.println("");
+    }
+
+  }
+}
