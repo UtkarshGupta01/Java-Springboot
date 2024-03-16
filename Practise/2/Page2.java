ae644323a0d472bba2ad6665a85d5eab1a7152c8
Utkarsh Gupta <utkarshgupta3008@gmail.com>
2024-03-16 14:30:10 +0530

    Page 2

---
+++ Practise/2/Page2.java
@@ -0,0 +1,15 @@
+// Sum and average of all elements in an array
+
+public class Page2 {
+  public static void main(String[] args) {
+    int[] arr = { 21, 3, 2, 12, 12, 7 };
+    int sum = 0;
+    for (int i : arr) {
+      sum += i;
+    }
+    double avg = (double) sum / arr.length;
+    System.out.println("Sum of all elements in an array : " + sum);
+    System.out.println("Average of all elements in an array : " + avg);
+  }
+
+}
