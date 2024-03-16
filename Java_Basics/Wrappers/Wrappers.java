d519a212b7c444165175a286b1249a30dfdcde55
Utkarsh Gupta <utkarshgupta3008@gmail.com>
2024-03-16 15:50:06 +0530

    Wrappers

---
+++ Java_Basics/Wrappers/Wrappers.java
@@ -0,0 +1,10 @@
+//Introduction to Wrapper Class
+
+public class Wrappers {
+  public static void main(String[] args) {
+    Integer x; // can also leave value unassigned
+    Integer a = 77; // becomes immutable
+    System.out.println(a.hashCode());
+  }
+
+}
