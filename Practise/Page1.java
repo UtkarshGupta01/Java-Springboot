5b77c1eef41795db3dc5747d31337a02976dcdb5
Utkarsh Gupta <utkarshgupta3008@gmail.com>
2024-04-02 16:08:57 +0530

    reverse array

--- Practise/Page1.java
+++ Practise/Page1.java
@@ -391,3 +391,28 @@
 // }
 
 // ==============================================================================
+
+//reverse an array
+
+public class Page1 {
+  public static void main(String[] args) {
+    int[] arr = { -1, 32, 12, 3, 4 };
+    int start = 0;
+    int end = arr.length - 1;
+    int temp;
+    System.out.println(start + "" + end);
+    do {
+      temp = arr[start];
+      arr[start] = arr[end];
+      arr[end] = temp;
+      start++;
+      end--;
+
+    } while (start < end);
+
+    for (int i : arr) {
+      System.out.print(" " + i);
+
+    }
+  }
+}
