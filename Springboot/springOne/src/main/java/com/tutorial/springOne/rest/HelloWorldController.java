a30b1398bd178629a11a300b2282a40e43376f28
Utkarsh Gupta <utkarshgupta3008@gmail.com>
2024-04-07 21:37:19 +0530

    Custom Application properties

--- Springboot/springOne/src/main/java/com/tutorial/springOne/rest/HelloWorldController.java
+++ Springboot/springOne/src/main/java/com/tutorial/springOne/rest/HelloWorldController.java
@@ -1,11 +1,25 @@
 package com.tutorial.springOne.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class HelloWorldController {
+//    inject properties from application.properties
+    @Value("${student.name}")
+    private String studentName;
+
+
+    @Value("${student.rollno}")
+    private String studentRollNumber;
+
+    @GetMapping("/studentInfo")
+    public String studentInfo(){
+        return "Student Name : " + studentName + " and Student Roll Number : " + studentRollNumber;
+    }
+
     @GetMapping("/")
     public String sayHello(){
         return "Hello Utkarsh! Welcome to learning Springboot :)";
