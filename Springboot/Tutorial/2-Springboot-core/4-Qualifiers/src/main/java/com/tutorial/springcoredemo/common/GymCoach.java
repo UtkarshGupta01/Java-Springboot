c08a04318e0e17cc02b39fc04f24f9c3887396b3
Utkarsh Gupta <utkarshgupta3008@gmail.com>
2024-04-09 11:30:03 +0530

    Qualifiers

---
+++ Springboot/Tutorial/2-Springboot-core/4-Qualifiers/src/main/java/com/tutorial/springcoredemo/common/GymCoach.java
@@ -0,0 +1,13 @@
+package com.tutorial.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GymCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout(){
+        return "Do 4 Sets of Chest press -  15 reps each !";
+    }
+
+}
---
+++ Springboot/Tutorial/2-Springboot-core/4-Qualifiers/src/main/java/com/tutorial/springcoredemo/common/TrackCoach.java
@@ -0,0 +1,11 @@
+package com.tutorial.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrackCoach implements Coach{
+    @Override
+    public String getDailyWorkout() {
+        return "Run for 10 minutes straight !";
+    }
+}
