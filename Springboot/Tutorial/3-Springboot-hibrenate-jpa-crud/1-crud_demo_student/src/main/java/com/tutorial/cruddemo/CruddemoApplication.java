1604003d94f02a9e46ac3a6dc6e71e27f3f560a3
Utkarsh Gupta <utkarshgupta3008@gmail.com>
2024-04-11 12:51:53 +0530

    Added multiple students

--- Springboot/Tutorial/3-Springboot-hibrenate-jpa-crud/1-crud_demo_student/src/main/java/com/tutorial/cruddemo/CruddemoApplication.java
+++ Springboot/Tutorial/3-Springboot-hibrenate-jpa-crud/1-crud_demo_student/src/main/java/com/tutorial/cruddemo/CruddemoApplication.java
@@ -17,9 +17,26 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
 		return runner->{
-			createStudent(studentDAO);
+//			createStudent(studentDAO);
+			createMultipleStudents(studentDAO);
 		};
 	}
+
+	private void createMultipleStudents(StudentDAO studentDAO) {
+		System.out.println("Creating 3 Student objects...");
+		Student tempStudent1 = new Student("Sneha","Shrivastava","snehawish2@gmail.com");
+		Student tempStudent2 = new Student("Pritish","Singh","pritishsingh26@gmail.com");
+		Student tempStudent3 = new Student("Tanya","Rathore","tanrath1111@gmail.com");
+
+		System.out.println("Saving the studnt...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
+
+		System.out.println("3 Students saved with  Id : "+ tempStudent1.getId()+" "+tempStudent2.getId()+" "+tempStudent3.getId());
+
+	}
+
 	private void createStudent(StudentDAO studentDAO){
 		System.out.println("Creating new Student object...");
 		Student tempStudent = new Student("Utkarsh","Gupta","ug53054@gmail.com");
