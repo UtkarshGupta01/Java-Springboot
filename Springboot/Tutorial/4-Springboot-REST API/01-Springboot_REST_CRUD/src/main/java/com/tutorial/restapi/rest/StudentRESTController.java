514bb37c4f9d315f6ef5093341c4f2d659378336
Utkarsh Gupta <utkarshgupta3008@gmail.com>
2024-04-15 18:10:38 +0530

    Global Exception Handing

--- Springboot/Tutorial/4-Springboot-REST API/01-Springboot_REST_CRUD/src/main/java/com/tutorial/restapi/rest/StudentRESTController.java
+++ Springboot/Tutorial/4-Springboot-REST API/01-Springboot_REST_CRUD/src/main/java/com/tutorial/restapi/rest/StudentRESTController.java
@@ -40,30 +40,5 @@ public class StudentRESTController {
 
         return theStudents.get(studentId);
     }
-    //Exception Handling
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
-
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimestamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
-
-    }
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
-
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exc.getMessage());
-        error.setTimestamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
-
-    }
 
 }
