8ddb066580bfaaa960b5bc75c1abdf0c76f3be4b
Utkarsh Gupta <utkarshgupta3008@gmail.com>
2024-04-30 14:38:10 +0530

    seach minimum

--- DSA Practise/Linear_Search/LinearSearch.java
+++ DSA Practise/Linear_Search/LinearSearch.java
@@ -7,14 +7,30 @@ public class LinearSearch {
 
   public static void main(String[] args) {
     int[] arr = {18,12,-17,2,3,14,28};
+
     int target = 3;
     int start = 1;
     int end = 6;
 
-    int ans = searchIndex(arr,target,start, end);
-    System.out.println("Target is present at the index : "+ans);
+//    int ans = searchIndex(arr,target,start, end);
+//    System.out.println("Target is present at the index : "+ans);
 
+    int min = searchMin(arr);
+    System.out.println("Minimum value in array : " + min);
 
+  }
+
+  public static int searchMin(int[] arr) {
+    int min = arr[0];
+    for(int i : arr){
+      if(i<min){
+        min = i;
+      }
+      else {
+        continue;
+      }
+    }
+    return min;
 
   }
 
